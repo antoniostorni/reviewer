727ee7bf4ff6e6629ef4b5219ae3fc0bcac5f566
Antonio Storni <antoniostorni@gmail.com>
2019-05-19 22:03:47 -0300

    Added basic admin, also, fixed verbose name plural for companies.

--- reviewer/reviews/admin.py
+++ reviewer/reviews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from reviewer.reviews.models import (Company,
+                                     Review,
+                                     )
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Review._meta.fields]
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Review, ReviewAdmin)
--- reviewer/reviews/models.py
+++ reviewer/reviews/models.py
@@ -18,6 +18,9 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "companies"
+
 
 class Review(models.Model):
     """
