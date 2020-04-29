1675b5ef164207391d877281f8dcab10ff742b67
Antonio Storni <antoniostorni@gmail.com>
2020-04-28 23:40:06 -0300

    Removed extra pasted code

--- reviewer/reviews/views.py
+++ reviewer/reviews/views.py
@@ -28,35 +28,3 @@ class ReviewView(generics.ListAPIView,
             return ReviewCreateSerializer
         else:
             return ReviewSerializer
-from rest_framework import serializers
-
-from .models import (Review,
-                     Company,
-                     )
-from reviewer.users.serializers import UserSerializer
-from .fields import CurrentUserIP
-
-
-class CompanySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Company
-        fields = '__all__'
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    reviewer = UserSerializer()
-    company = CompanySerializer()
-
-    class Meta:
-        model = Review
-        fields = '__all__'
-
-
-class ReviewCreateSerializer(serializers.ModelSerializer):
-    reviewer = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    ip_address = serializers.HiddenField(default=CurrentUserIP())
-
-    class Meta:
-        model = Review
-        fields = ('title', 'company', 'summary', 'rating', 'reviewer', 'ip_address')
