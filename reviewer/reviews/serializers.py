11d8adf108f608b876f0209cb9840f0f952db79b
Antonio Storni <antoniostorni@gmail.com>
2020-04-28 23:38:45 -0300

    Updated serializers.py

--- reviewer/reviews/serializers.py
+++ reviewer/reviews/serializers.py
@@ -5,12 +5,6 @@ from .models import (Review,
                      )
 from reviewer.users.serializers import UserSerializer
 from django.contrib.auth import get_user_model
-from django.db import models
-import uuid
-from django.core.validators import (MinValueValidator,
-                                    MaxValueValidator,
-                                    )
-from .fields import CurrentUserIP
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -36,39 +30,3 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ('title', 'company', 'summary', 'rating', 'reviewer', 'ip_address')
-
-        
-class Company(models.Model):
-    """
-    Company model
-    """
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    name = models.CharField(max_length=120)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "companies"
-
-
-class Review(models.Model):
-    """
-    Review model
-    """
-
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    rating = models.PositiveIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    title = models.CharField(max_length=64)
-    summary = models.TextField(max_length=10000)
-    ip_address = models.GenericIPAddressField()
-    date = models.DateTimeField(auto_now_add=True, editable=False)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    reviewer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.reviewer} - {self.company}'
-
-    class Meta:
-        # Allow only one review per user per restaurant
-        unique_together = [['reviewer', 'company']]
